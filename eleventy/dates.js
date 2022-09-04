0ad0715238e85aeee0786b27ffe92e9dfcc9d2d7
neonew <neonew@gmail.com>
2022-09-04 11:27:52 +0200

    Postlist styling

--- eleventy/dates.js
+++ eleventy/dates.js
@@ -1,7 +1,7 @@
 const { DateTime } = require("luxon")
 
 module.exports = {
-  date: dateObject => DateTime.fromJSDate(dateObject).setLocale("de").toLocaleString(DateTime.DATE_FULL),
+  date: dateObject => DateTime.fromJSDate(dateObject).setLocale("en").toLocaleString(DateTime.DATE_MED),
   isoDate: dateObject => DateTime.fromJSDate(dateObject).toISODate(),
   isoDateTime: dateObject => DateTime.fromJSDate(dateObject).toISO()
 }
