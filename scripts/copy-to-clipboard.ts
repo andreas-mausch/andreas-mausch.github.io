eb914079f55b56160979d6df1caf1af162da23bd
neonew <neonew@gmail.com>
2022-12-07 02:43:53 +0100

    Show checkmark as confirmation on successful copy-to-clipboard

--- scripts/copy-to-clipboard.ts
+++ scripts/copy-to-clipboard.ts
@@ -2,7 +2,19 @@ export function copyToClipboard() {
   const divs = document.querySelectorAll(".copy-to-clipboard")
 
   divs.forEach(element => element.addEventListener("click", event => {
+    const button = (event.target as Element).closest("button")
+    const i = button.firstChild as Element
     const code = (event.target as Element).closest("code")
     navigator.clipboard.writeText(code.innerText)
+
+    button.disabled = true
+    i.classList.remove("icon-copy")
+    i.classList.add("icon-check")
+
+    setTimeout(() => {
+      button.disabled = false
+      i.classList.remove("icon-check")
+      i.classList.add("icon-copy")
+    }, 3000)
   }))
 }
