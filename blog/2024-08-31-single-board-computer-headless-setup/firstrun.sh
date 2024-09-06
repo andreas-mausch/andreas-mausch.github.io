ac9aaaf3c50e1c2550b63a2e7b2f8509170a8fc2
andreas-mausch <andreas-mausch@users.noreply.github.com>
2024-09-06 16:32:50 +0000

    deploy: 31d1a8d5600d4d1f341459705efbd45cbac61b6c

--- blog/2024-08-31-single-board-computer-headless-setup/firstrun.sh
+++ blog/2024-08-31-single-board-computer-headless-setup/firstrun.sh
@@ -2,54 +2,68 @@
 
 set +e
 
+source firstrun.env
+
 CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
 if [ -f /usr/lib/raspberrypi-sys-mods/imager_custom ]; then
-   /usr/lib/raspberrypi-sys-mods/imager_custom set_hostname mycustomhost
+   /usr/lib/raspberrypi-sys-mods/imager_custom set_hostname "${MY_HOSTNAME}"
 else
-   echo mycustomhost >/etc/hostname
-   sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\tmycustomhost/g" /etc/hosts
+   echo "${MY_HOSTNAME}" >/etc/hostname
+   sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t${MY_HOSTNAME}/g" /etc/hosts
 fi
 FIRSTUSER=`getent passwd 1000 | cut -d: -f1`
 FIRSTUSERHOME=`getent passwd 1000 | cut -d: -f6`
 if [ -f /usr/lib/raspberrypi-sys-mods/imager_custom ]; then
-   /usr/lib/raspberrypi-sys-mods/imager_custom enable_ssh -k 'ssh-rsa AAAAB3Nz... openpgp:0x01234567'
+   if [ -n "${MY_SSH_KEY}" ]; then
+      /usr/lib/raspberrypi-sys-mods/imager_custom enable_ssh -k "${MY_SSH_KEY}"
+   else
+      /usr/lib/raspberrypi-sys-mods/imager_custom enable_ssh
+   fi
 else
-   install -o "$FIRSTUSER" -m 700 -d "$FIRSTUSERHOME/.ssh"
-   install -o "$FIRSTUSER" -m 600 <(printf "ssh-rsa AAAAB3Nz... openpgp:0x01234567") "$FIRSTUSERHOME/.ssh/authorized_keys"
-   echo 'PasswordAuthentication no' >>/etc/ssh/sshd_config
+   if [ -n "${MY_SSH_KEY}" ]; then
+      install -o "$FIRSTUSER" -m 700 -d "$FIRSTUSERHOME/.ssh"
+      install -o "$FIRSTUSER" -m 600 <(printf "${MY_SSH_KEY}") "$FIRSTUSERHOME/.ssh/authorized_keys"
+      echo 'PasswordAuthentication no' >>/etc/ssh/sshd_config
+   fi
    systemctl enable ssh
 fi
 if [ -f /usr/lib/userconf-pi/userconf ]; then
-   /usr/lib/userconf-pi/userconf 'mypiuser' '$5$OF...'
+   if [ -n "${MY_USER_PASSWORD}" ]; then
+      /usr/lib/userconf-pi/userconf "${MY_USER_NAME}" "${MY_USER_PASSWORD}"
+   else
+      /usr/lib/userconf-pi/userconf "${MY_USER_NAME}"
+   fi
 else
-   echo "$FIRSTUSER:"'$5$OF...' | chpasswd -e
-   if [ "$FIRSTUSER" != "mypiuser" ]; then
-      usermod -l "mypiuser" "$FIRSTUSER"
-      usermod -m -d "/home/mypiuser" "mypiuser"
-      groupmod -n "mypiuser" "$FIRSTUSER"
+   if [ -n "${MY_USER_PASSWORD}" ]; then
+      echo "$FIRSTUSER:${MY_USER_PASSWORD}" | chpasswd -e
+   fi
+   if [ "$FIRSTUSER" != "${MY_USER_NAME}" ]; then
+      usermod -l "${MY_USER_NAME}" "$FIRSTUSER"
+      usermod -m -d "/home/${MY_USER_NAME}" "${MY_USER_NAME}"
+      groupmod -n "${MY_USER_NAME}" "$FIRSTUSER"
       if grep -q "^autologin-user=" /etc/lightdm/lightdm.conf ; then
-         sed /etc/lightdm/lightdm.conf -i -e "s/^autologin-user=.*/autologin-user=mypiuser/"
+         sed /etc/lightdm/lightdm.conf -i -e "s/^autologin-user=.*/autologin-user=${MY_USER_NAME}/"
       fi
       if [ -f /etc/systemd/system/getty@tty1.service.d/autologin.conf ]; then
-         sed /etc/systemd/system/getty@tty1.service.d/autologin.conf -i -e "s/$FIRSTUSER/mypiuser/"
+         sed /etc/systemd/system/getty@tty1.service.d/autologin.conf -i -e "s/$FIRSTUSER/${MY_USER_NAME}/"
       fi
       if [ -f /etc/sudoers.d/010_pi-nopasswd ]; then
-         sed -i "s/^$FIRSTUSER /mypiuser /" /etc/sudoers.d/010_pi-nopasswd
+         sed -i "s/^$FIRSTUSER /${MY_USER_NAME} /" /etc/sudoers.d/010_pi-nopasswd
       fi
    fi
 fi
 if [ -f /usr/lib/raspberrypi-sys-mods/imager_custom ]; then
-   /usr/lib/raspberrypi-sys-mods/imager_custom set_wlan 'mywifi' 'e1251c1c8c480c086bc193999dcde00004e3d30f91f8d421d437d712c71ad1b7' 'DE'
+   /usr/lib/raspberrypi-sys-mods/imager_custom set_wlan "${MY_WIFI_SSID}" "${MY_WIFI_PASSWORD}" "${MY_WIFI_COUNTRY}"
 else
-cat >/etc/wpa_supplicant/wpa_supplicant.conf <<'WPAEOF'
-country=DE
+envsubst >/etc/wpa_supplicant/wpa_supplicant.conf <<'WPAEOF'
+country=${MY_WIFI_COUNTRY}
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 ap_scan=1
 
 update_config=1
 network={
-	ssid="mywifi"
-	psk=e1251c1c8c480c086bc193999dcde00004e3d30f91f8d421d437d712c71ad1b7
+	ssid="${MY_WIFI_SSID}"
+	psk=${MY_WIFI_PASSWORD}
 }
 
 WPAEOF
@@ -60,15 +74,15 @@ WPAEOF
    done
 fi
 if [ -f /usr/lib/raspberrypi-sys-mods/imager_custom ]; then
-   /usr/lib/raspberrypi-sys-mods/imager_custom set_keymap 'us'
-   /usr/lib/raspberrypi-sys-mods/imager_custom set_timezone 'Europe/Berlin'
+   /usr/lib/raspberrypi-sys-mods/imager_custom set_keymap "${MY_KEYBOARD_LAYOUT}"
+   /usr/lib/raspberrypi-sys-mods/imager_custom set_timezone "${MY_TIMEZONE}"
 else
    rm -f /etc/localtime
-   echo "Europe/Berlin" >/etc/timezone
+   echo "${MY_TIMEZONE}" >/etc/timezone
    dpkg-reconfigure -f noninteractive tzdata
-cat >/etc/default/keyboard <<'KBEOF'
+envsubst >/etc/default/keyboard <<'KBEOF'
 XKBMODEL="pc105"
-XKBLAYOUT="us"
+XKBLAYOUT="${MY_KEYBOARD_LAYOUT}"
 XKBVARIANT=""
 XKBOPTIONS=""
 
